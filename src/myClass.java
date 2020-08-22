9957b67e44cd54d07678e89f0d46d41bad9a6ef1
mudit <mudit.shukla_cs19@gla.ac.in>
2020-08-22 16:00:52 +0530

    creating main method in myClass.java file

---
+++ src/myClass.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Mudit Shukla
+ *   Date: 22-08-2020
+ *   Time: 15:59
+ *   File: myClass.java
+ */
+
+public class myClass {
+    public static void main(String[] args) {
+
+    }
+}
